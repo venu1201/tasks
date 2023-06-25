8ca26101ad436eaff8a1780a17584b5a27d48346
Venusai <89316212+venu1201@users.noreply.github.com>
2023-06-26 00:26:30 +0530

    second

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/mern_todo', {
+mongoose.connect('mongodb://127.0.0.1:27017/mern_form', {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true 
 }).then(() => console.log("Connected to MongoDB")).catch(console.error);
